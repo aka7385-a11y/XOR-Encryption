a061c75c57ab8b8a8f7b58caf1c6fe473028d55b
Ash10 <aka7385@g.rit.edu>
2026-09-20 21:06:27 -0400

    Implement empty key input test validation
    Added input validation for the key to ensure it is not empty.

--- xor.c
+++ xor.c
@@ -39,9 +39,16 @@ int main() {
         fgets(filename, MAX_KEY_LENGTH, stdin);
         filename[strcspn(filename, "\n")] = '\0'; // Remove newline
 
-        printf("Enter the key: ");
-        fgets(key, MAX_KEY_LENGTH, stdin);
-        key[strcspn(key, "\n")] = '\0'; // Remove newline
+              do {
+          printf("Enter the key: ");
+          if (fgets(key, MAX_KEY_LENGTH, stdin) != NULL) { // if messgae is empty 
+              key[strcspn(key, "\n")] = '\0'; // Remove newline
+              if (strlen(key) == 0) {
+                  printf("Input empty, try again\n");
+              }
+          }
+      } while (strlen(key) == 0);
+
 
         FILE *file = fopen(filename, "w");
         if (file == NULL) {
